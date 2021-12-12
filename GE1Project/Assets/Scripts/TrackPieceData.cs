6d10bb8ee0bf0aeb02a77049f8dc48e4178944ad
conorm <conorm456@hotmail.com>
2021-12-12 16:36:22 +0000

    TrackGen Scripts created

---
+++ GE1Project/Assets/Scripts/TrackPieceData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackPieceData : MonoBehaviour
+{
+
+    enum Direction
+    {
+        North, South, East, West
+    }
+
+    public Vector2 pieceSize = new Vector2(10f,10f);
+    public GameObject[] levelPieces;
+    public Direction startDir;
+    public Direction endDir;
+}
