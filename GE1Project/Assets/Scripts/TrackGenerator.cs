b7266c1e88d02f1357d7c9e84e0f2465ecf10e20
conorm <conorm456@hotmail.com>
2021-12-12 17:06:53 +0000

    Added func to get the next piece of track

--- GE1Project/Assets/Scripts/TrackGenerator.cs
+++ GE1Project/Assets/Scripts/TrackGenerator.cs
@@ -5,15 +5,46 @@ using UnityEngine;
 public class TrackGenerator : MonoBehaviour
 {
 
+    public TrackPieceData[] trackPieceData;
+    public TrackPieceData firstPiece;
+
+    private TrackPieceData prevPiece;
+
+    public Vector3 spawnOrg;
+
+    private Vector3 spawnPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        prevPiece = firstPiece;
+
+        for (int i = 0; i < length; i++)
+        {
+            //spawn track
+        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //select which piece will come next
+    TrackPieceData GetNextPiece()
+    {
+        List<TrackPieceData> eligbleTrackPiece = new List<TrackPieceData>();
+
+        TrackPieceData.Direction nextDir = TrackPieceData.Direction.North;
+
+        switch (prevPiece.endDir)
+        {
+            case TrackPieceData.Direction.North:
+            case TrackPieceData.Direction.South:
+            case TrackPieceData.Direction.East:
+            case TrackPieceData.Direction.West:
+            default:
+        }
     }
 }
--- GE1Project/Assets/Scripts/TrackPieceData.cs
+++ GE1Project/Assets/Scripts/TrackPieceData.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class TrackPieceData : MonoBehaviour
 {
-
     enum Direction
     {
         North, South, East, West
     }
 
     public Vector2 pieceSize = new Vector2(10f,10f);
+    
     public GameObject[] levelPieces;
     public Direction startDir;
     public Direction endDir;
