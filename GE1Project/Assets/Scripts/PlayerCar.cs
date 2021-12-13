d29b97c6205f8f60ce4571a84722ed49af37ff05
conorm <conorm456@hotmail.com>
2021-12-13 16:24:50 +0000

    added movement to car

--- GE1Project/Assets/Scripts/PlayerCar.cs
+++ GE1Project/Assets/Scripts/PlayerCar.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class PlayerCar : MonoBehaviour
 {
-    private float accel = 10;
-    private float deccel = 15;
-    private float maxSpeed = 100;
+    public float accel = 10;
+    public float deccel = 15;
+    public float maxSpeed = 100;
     private float speed = 0;
 
     private float steerInput;
@@ -19,11 +19,25 @@ public class PlayerCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Vertical"))
+        if(Input.GetButton("Vertical") && speed<maxSpeed)
         {
             speed = speed - (accel * Time.deltaTime);
         }
 
+        else
+        {
+            if(speed > (deccel * Time.deltaTime))
+            {
+                speed = speed - (deccel * Time.deltaTime);
+            }
+            else
+            {
+                speed = 0;
+            }
+        }
+
         steerInput = Input.GetAxis("Horizontal");
+
+        transform.position = new Vector3(transform.position.x + speed, transform.position.y , transform.position.z);
     }
 }
