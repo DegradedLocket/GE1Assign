a4cfc39b2ea6fdd3d2968c31c4cad3c1ee53fda2
conorm <conorm456@hotmail.com>
2021-12-13 18:19:16 +0000

    Changed accel logic

--- GE1Project/Assets/Scripts/PlayerCar.cs
+++ GE1Project/Assets/Scripts/PlayerCar.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerCar : MonoBehaviour
 {
-    public float accel = 10;
+    public bool accel = 10;
     public float deccel = 15;
     public float maxSpeed = 100;
     public float speed = 0;
